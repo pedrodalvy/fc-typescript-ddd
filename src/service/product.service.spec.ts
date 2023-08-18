c0ad09becb7d3633f3733ce38f846bf1dc6104fe
Pedro Dalvy <pedrodalvy@outlook.com>
2023-08-17 22:15:21 -0300

    feat: create product service to increase products price

---
+++ src/service/product.service.spec.ts
@@ -0,0 +1,18 @@
+import Product from '../entity/product';
+import ProductService from './product.service';
+
+describe('Product service unit tests', () => {
+  it('should change the price of all products', async () => {
+    // ARRANGE
+    const product1 = new Product('product_1', 'Product 1', 10);
+    const product2 = new Product('product_2', 'Product 2', 20);
+    const products = [product1, product2];
+
+    // ACT
+    ProductService.increasePrice(products, 100);
+
+    // ASSERT
+    expect(product1.price).toBe(20);
+    expect(product2.price).toBe(40);
+  });
+});

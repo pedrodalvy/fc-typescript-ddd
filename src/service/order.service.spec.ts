03fa72de35ad961e9a91eb61c41a1966e25b6816
Pedro Dalvy <pedrodalvy@outlook.com>
2023-08-18 21:12:51 -0300

    chore: correct typos in test and update tsconfig options

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -6,11 +6,11 @@ import Customer from '../entity/customer';
 describe('Order service unit tests', () => {
   it('should get total of all orders', () => {
     // ARRANGE
-    const intem1 = new OrderItem('item1', 'Item 1', 100, 'product1', 1);
-    const order1 = new Order('order1', 'customerId1', [intem1]);
+    const item1 = new OrderItem('item1', 'Item 1', 100, 'product1', 1);
+    const order1 = new Order('order1', 'customerId1', [item1]);
 
-    const intem2 = new OrderItem('item2', 'Item 2', 200, 'product2', 2);
-    const order2 = new Order('order2', 'customerId2', [intem2]);
+    const item2 = new OrderItem('item2', 'Item 2', 200, 'product2', 2);
+    const order2 = new Order('order2', 'customerId2', [item2]);
 
     // ACT
     const total = OrderService.total([order1, order2]);

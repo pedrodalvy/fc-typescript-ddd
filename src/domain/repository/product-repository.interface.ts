336161ba43e98af5ed000dea2720cd6169304dc0
Pedro Dalvy <pedrodalvy@outlook.com>
2023-08-23 22:18:33 -0300

    feat: add product repository

---
+++ src/domain/repository/product-repository.interface.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from './repository.interface';
+import Product from '../entity/product';
+
+export interface ProductRepositoryInterface extends RepositoryInterface<Product> {}
